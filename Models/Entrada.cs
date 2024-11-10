e4314f854ec9f236f5147bb3d86b80623f433a04
Patricia <patricia.abadaranda@gmail.com>
2024-11-10 16:58:34 +0100

    Contador autoincremental del id agregado a la clase Entrada.

--- Models/Entrada.cs
+++ Models/Entrada.cs
@@ -1,20 +1,16 @@
 namespace Models;
 
 public class Entrada {
-
+    private static int contadorId = 1;
     public int Id {get; set;}
-
     public string Tipo {get; set;} = "";
-
     public int Precio {get; set;}
-
-    public Entrada(int id, string tipo, int precio) {
-        Id = id;
+    public Entrada(string tipo, int precio) {
+        Id = contadorId++;
         Tipo = tipo;
         Precio = precio;
         if (precio < 0) {
             throw new ArgumentException("Error: El precio no puede ser negativo");
         }
     }
-
 }
