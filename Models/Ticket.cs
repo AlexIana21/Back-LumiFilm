70ab67355e1e9590d47a6b1318cda93dc665935f
Patricia <patricia.abadaranda@gmail.com>
2024-11-10 12:54:23 +0100

    Atributos agregados a Ticket

--- Models/Ticket.cs
+++ Models/Ticket.cs
@@ -1,14 +1,13 @@
 namespace Models;
 
 public class Ticket {
-
     private static int contadorId = 1;
     public int Id {get; private set;}
-
+    public Pelicula Pelicula {get; set;}
+    public Sala Sala {get; set;}
+    public List<Asiento> AsientosReservados { get; private set; }
     private List<(Entrada entrada, int cantidad)> entradas;
-
     public DateTime FechaTicket {get; private set;}
-
     public double Total {
         get {
             double totalPedido = 0;
@@ -19,13 +18,15 @@ public class Ticket {
         }
     }
 
-    public static int TotalTickets {get; private set;}
+    public static int TotalTickets {get; private set;} = 1;
 
-    public Ticket() {
+    public Ticket(Pelicula pelicula, Sala sala) {
         Id = contadorId++;
+        Pelicula = pelicula;
+        Sala = sala;
+        AsientosReservados = new List<Asiento>();
         entradas = new List<(Entrada entrada, int cantidad)>();
         FechaTicket = DateTime.Now;
         TotalTickets++;
     }
-    
 }
