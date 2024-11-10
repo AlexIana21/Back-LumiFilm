89309e6b800c49032d61ab0a504f908e746f6547
Patricia <patricia.abadaranda@gmail.com>
2024-11-10 09:49:18 +0100

    Carpeta Models

--- obj/Debug/net8.0/Reto-Back.AssemblyInfo.cs
+++ obj/Debug/net8.0/Reto-Back.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Reto-Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db36447aea63f1cb29e427450c251f64ffc7100")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9e3940e15040fefa7a4cec4700075b332d6295")]
 [assembly: System.Reflection.AssemblyProductAttribute("Reto-Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Reto-Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
