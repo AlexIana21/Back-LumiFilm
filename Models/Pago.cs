fd6a0a79547543deeefeabd939247faf9e651fac
Patricia <patricia.abadaranda@gmail.com>
2024-11-10 12:36:52 +0100

    Contador pagos id

--- Models/Pago.cs
+++ Models/Pago.cs
@@ -1,21 +1,17 @@
-public class Pago
-{
-   public static int Id {get; private set;} = 1;
-    
+namespace Models;
+public class Pago {
+    private static int contadorId = 1;
+    public int Id {get; private set;} = 1;
     public string Nombre  { get; set; }
-
     public string Apellido  { get; set; }
     public int Telefono   { get; set; }
     public string Mail  { get; set; }
 
-
-      public Pago(string nombre, string apellido, int telefono, string mail) {
-        Id++;
-        Nombre = nombre;
-        Apellido = apellido;
-        Telefono = telefono;
-        Mail = mail;
-        
+    public Pago(string nombre, string apellido, int telefono, string mail) {
+      Id = contadorId++;
+      Nombre = nombre;
+      Apellido = apellido;
+      Telefono = telefono;
+      Mail = mail;
     }
-
 }
