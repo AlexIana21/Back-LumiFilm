26bb62afb1b5e46705caf427e31099d879a98afb
Patricia <patricia.abadaranda@gmail.com>
2024-11-10 10:47:07 +0100

    Clase pelicula

--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -1,1 +1,22 @@
- 
+namespace Models;
+
+public class Pelicula {
+    
+    public string Titulo {get;set;} = "";
+
+    public string Sinopsis {get;set;} = "";
+
+    public int Duracion {get;set;}
+
+    public int Clasificacion {get;set;}
+
+    public string Genero {get;set;} = "";
+
+    public Pelicula(string titulo, string sinopsis, int duracion, int clasificacion, string genero) {
+        Titulo = titulo;
+        Sinopsis = sinopsis;
+        Duracion = duracion;
+        Clasificacion = clasificacion;
+        Genero = genero;
+    }
+}
