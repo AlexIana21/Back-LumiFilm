623d1e0ab1a21f2f340caea1e8b83e0bfa9532bd
Patricia <patricia.abadaranda@gmail.com>
2024-11-10 12:34:32 +0100

    Contador pelicula id

--- Models/Pelicula.cs
+++ Models/Pelicula.cs
@@ -1,21 +1,16 @@
 namespace Models;
 
 public class Pelicula {
-
-    public int Id {get; set;}
-    
+    private static int contadorId = 1;
+    public int Id {get; private set;}
     public string Titulo {get;set;} = "";
-
     public string Sinopsis {get;set;} = "";
-
     public int Duracion {get;set;}
-
     public int Clasificacion {get;set;}
-
     public string Genero {get;set;} = "";
 
-    public Pelicula(int id, string titulo, string sinopsis, int duracion, int clasificacion, string genero) {
-        Id = id;
+    public Pelicula(string titulo, string sinopsis, int duracion, int clasificacion, string genero) {
+        Id = contadorId++;
         Titulo = titulo;
         Sinopsis = sinopsis;
         Duracion = duracion;
