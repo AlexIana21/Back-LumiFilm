ce9b789e9e26f158061b831f9190715893b1b101
Patricia <patricia.abadaranda@gmail.com>
2025-01-26 10:02:47 +0100

    add model

---
+++ Models/Admin.cs
@@ -0,0 +1,13 @@
+using Models;
+
+public class Admin {
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Mail { get; set; }
+    public string Password { get; set; }
+
+    public Admin () {
+    }
+
+}
